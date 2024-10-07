a28f2e3e75e6e8990d03439d6757b6799c04560a
Shourov Soron <shourovhossainsoron@gmail.com>
2024-10-07 13:00:00 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+//For Navigation
+
+
+let mobileMenuBtn = document.querySelector(".mobile-menu-btn");
+
+const primaryNav=document.querySelector(".primary-navigation");
+
+
+
+mobileMenuBtn.addEventListener('click', ()=>{
+    primaryNav.toggleAttribute("data-visible");
+
+    if(primaryNav.hasAttribute("data-visible")){
+        mobileMenuBtn.innerHTML='<i class="fa-solid fa-xmark"></i>'
+    }else{
+      
+       mobileMenuBtn.innerHTML='<i class="fa-solid fa-bars"></i>'
+    }
+});
+
+
+//Navigation End
